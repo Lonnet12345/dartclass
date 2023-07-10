c48f78c83c81238c1b2510d09fc5a3e31eb6327f
Lonnet12345 <lonnetniwahereza@gmail.com>
2023-07-10 16:55:03 +0300

    my dart class about how to use variables

---
+++ bin/myproject.dart
@@ -0,0 +1,11 @@
+import 'package:myproject/myproject.dart' as myproject;
+import 'package:myproject/myproject.dart';
+
+void main(List<String> arguments) {
+  print('Hello world: ${myproject.calculate()}!');
+}
+
+void main() {
+  var x = calculate();
+  print(x);
+}
