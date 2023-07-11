d55518d3859daf6639a763259b4fe1880fa6180e
Lonnet12345 <lonnetniwahereza@gmail.com>
2023-07-11 12:20:45 +0300

    arithmetic operators  class

---
+++ lib/operators.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+void main() {
+  // var university = "bishop stuart university";
+  // print(university);
+
+//
+  int a = 10;
+  int y = 5;
+//addition:a + y = 15
+//subtraction:a - y =5
+//multiplication:a * y = 25
+//division:a / y = 2
+//modulous:a % y =0.15
+  print("addition a + y :${a+y}");
+  print("subtraction a-y :${a-y}");
+  print("multiplication a * y :${a+y}");
+  print("division a / y :${a+y}");
+  print("modulus a % y :${a+y}");
+}
